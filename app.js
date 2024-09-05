ec798318e078f984d8797205f862c2ff62ca6927
Imri Neeman <neemanimri@gmail.com>
2024-09-05 14:02:45 +0300

    Added commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/liveness', (req,res) => {
+res.send('OK')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
